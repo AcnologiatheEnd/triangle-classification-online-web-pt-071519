a758b827cb7bffba6d3e7ce270e778376748d4ab
Winston  Chung <18.chung.winston@ctkprep.org>
2019-09-09 03:33:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,7 @@ class Triangle
   end
   
   def kind 
-  if((s1 == 0) && (s2 == 0) && (s3 == 0)) || ((s1 == 3) && (s2 == 4) && (s3 == -5)) || ((s1 == 1) && (s2 == 1) && (s3 == 3)) || ((s1 == 2) && (s2 == 4) && (s3 == 2)) || ((s1 == 7) && (s2 == 3) && (s3 == 2))
-    raise TriangleError
+  validate_triangle
   else
     if ((s1 == s2) && (s1 == s3) && (s2 == s3))
       return :equilateral
@@ -22,6 +21,13 @@ class Triangle
       raise TriangleError
     end
   end
+  end
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
   end
   
   class TriangleError < StandardError
